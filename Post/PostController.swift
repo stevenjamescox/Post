0daae4ca683f2ad53256ea97600b4ed9ee29444b
Caleb Hicks <calebhicks@gmail.com>
2016-05-19 15:24:56 -0600

    part 2 solution

--- Post/PostController.swift
+++ Post/PostController.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class PostController {
     
-    static let endpoint = NSURL(string: "https://devmtn-post.firebaseio.com/posts.json")
+    static let baseURL = NSURL(string: "https://devmtn-post.firebaseio.com/posts/")
+    static let endpoint = baseURL?.URLByAppendingPathExtension("json")
     
     weak var delegate: PostControllerDelegate?
     
@@ -24,13 +25,47 @@ class PostController {
         fetchPosts()
     }
     
+    // MARK: - Save Post
+    
+    func addPost(username: String, text: String) {
+        
+        let post = Post(username: username, text: text)
+        
+        guard let requestURL = post.endpoint else { fatalError("URL optional is nil") }
+        
+        NetworkController.performRequestForURL(requestURL, httpMethod: .Put, body: post.jsonData) { (data, error) in
+            
+            let responseDataString = NSString(data: data!, encoding: NSUTF8StringEncoding) ?? ""
+            
+            if error != nil {
+                print("Error: \(error)")
+            } else if responseDataString.containsString("error") {
+                print("Error: \(responseDataString)")
+            } else {
+                print("Successfully saved data to endpoint. \nResponse: \(responseDataString)")
+            }
+            
+            self.fetchPosts()
+        }
+    }
+    
     // MARK: - Request
     
-    func fetchPosts(completion: ((newPosts: [Post]) -> Void)? = nil) {
+    func fetchPosts(reset reset: Bool = true, completion: ((newPosts: [Post]) -> Void)? = nil) {
     
         guard let requestURL = PostController.endpoint else { fatalError("Post Endpoint url failed") }
         
-        NetworkController.performRequestForURL(requestURL, httpMethod: .Get, urlParameters: nil) { (data, error) in
+        let queryEndInterval = reset ? NSDate().timeIntervalSince1970 : posts.last?.queryTimestamp ?? NSDate().timeIntervalSince1970
+        
+        //TODO update to query timestamp
+        
+        let urlParameters = [
+            "orderBy": "\"timestamp\"",
+            "endAt": "\(queryEndInterval)",
+            "limitToLast": "15",
+            ]
+        
+        NetworkController.performRequestForURL(requestURL, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
         
             let responseDataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             
@@ -49,7 +84,11 @@ class PostController {
             
             dispatch_async(dispatch_get_main_queue(), {
                 
-                self.posts = sortedPosts
+                if reset {
+                    self.posts = sortedPosts
+                } else {
+                    self.posts.appendContentsOf(sortedPosts)
+                }
                 
                 if let completion = completion {
                     completion(newPosts: sortedPosts)
--- Post/PostListTableViewController.swift
+++ Post/PostListTableViewController.swift
@@ -23,15 +23,20 @@ class PostListTableViewController: UITableViewController {
         postController.delegate = self
     }
     
+    @IBAction func addPostTapped(sender: AnyObject) {
+        
+        presentNewPostAlert()
+    }
+    
     @IBAction func refreshControlPulled(sender: UIRefreshControl) {
         
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         
-        postController.fetchPosts({ (newPosts) in
+        postController.fetchPosts(reset: true) { (newPosts) in
             sender.endRefreshing()
             
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-        })
+        }
     }
 
     
@@ -53,6 +58,71 @@ class PostListTableViewController: UITableViewController {
         return cell
     }
     
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row+1 == postController.posts.count {
+            
+            postController.fetchPosts(reset: false, completion: { (newPosts) in
+                
+                if !newPosts.isEmpty {
+                    
+                    self.tableView.reloadData()
+                }
+            })
+        }
+    }
+    
+    
+    // MARK: - Alert Controllers
+    
+    func presentNewPostAlert() {
+        let alertController = UIAlertController(title: "New Post", message: nil, preferredStyle: .Alert)
+        
+        var usernameTextField: UITextField?
+        var messageTextField: UITextField?
+        
+        alertController.addTextFieldWithConfigurationHandler { (usernameField) in
+            usernameField.placeholder = "Display name"
+            usernameTextField = usernameField
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (messageField) in
+            
+            messageField.placeholder = "What's up?"
+            messageTextField = messageField
+        }
+        
+        let postAction = UIAlertAction(title: "Post", style: .Default) { (action) in
+            
+            guard let username = usernameTextField?.text where !username.isEmpty,
+                let text = messageTextField?.text where !text.isEmpty else {
+                    
+                    self.presentErrorAlert()
+                    return
+            }
+            
+            self.postController.addPost(username, text: text)
+        }
+        alertController.addAction(postAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func presentErrorAlert() {
+        
+        let alertController = UIAlertController(title: "Uh oh!", message: "You may be missing information or have network connectivity issues. Please try again.", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
 }
 
 extension PostListTableViewController: PostControllerDelegate {
